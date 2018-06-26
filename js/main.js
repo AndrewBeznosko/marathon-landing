c2ffba6235628457f1d81762ef18b574a5cb9840
Anton <anton.vakulenchyk@gmail.com>
2018-06-26 09:47:20 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ $(document).ready(function(){
     });
 
 });
+
+$(document).ready(function(){
+
+    $("#headermenu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор блока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+
